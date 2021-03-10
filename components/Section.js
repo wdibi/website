3a0de1b64cfe79928cd9b3fec7673f8afc3bb729
Will DiBiagio <wdibiagio@gmail.com>
2021-03-09 18:05:57 -0800

    create reusable sections

---
+++ components/Section.js
@@ -0,0 +1,28 @@
+function SectionItem({ text, linkText, href }) {
+  return (
+    <li>
+      {text}{' '}
+      <mark>
+        <a href={href}>{linkText}</a>
+      </mark>
+      <div class="small"></div>
+    </li>
+  );
+}
+
+export default function Section({ title, items }) {
+  return (
+    <details>
+      <summary>
+        <h2 class="as-text">
+          <i>_{title.toUpperCase()}</i>
+        </h2>
+      </summary>
+      <ul class="list list-items">
+        {items.map((item) => (
+          <SectionItem {...item} />
+        ))}
+      </ul>
+    </details>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,44 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Section from '../components/Section';
+
+const content = [
+  {
+    title: 'education',
+    items: [
+      {
+        text: 'Studying Computer Science at',
+        linkText: 'LMU',
+        href: 'https://lmu.edu',
+      },
+    ],
+  },
+  { title: 'work', items: [] },
+  { title: 'projects', items: [] },
+];
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>Will DiBiagio</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Cousine:400,700"
+          rel="stylesheet"
+          type="text/css"
+        />
       </Head>
+      <div className="theme" />
+      <header className="header">
+        <h1>Will DiBiagio</h1>
+      </header>
 
-      <main className={styles.main}>
-        <div className={styles.wrapper}>
-          <div className={styles.typing}>hey, it's will.</div>
-        </div>
+      <main className="wrapper">
+        {content.map((section) => (
+          <Section {...section} />
+        ))}
       </main>
+      <footer className="footer">That is all.</footer>
     </div>
   );
 }
