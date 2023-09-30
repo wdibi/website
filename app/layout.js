aa01ce4b056cf68e92cf4d2e369b0224444492ee
Will DiBiagio <wdibiagio@gmail.com>
2023-09-30 12:54:14 -0700

    use app dir

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+import '@/styles/globals.css';
+import {Cousine} from '@next/font/google';
+
+export const metadata = {
+  title: 'Will DiBiagio',
+  description: '',
+};
+
+export const cousine = Cousine({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
+export default function RootLayout({children}) {
+  return (
+    <html lang="en">
+      <body className={cousine.className}>{children}</body>
+    </html>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,19 +1,9 @@
-import Head from 'next/head';
-import Section from '../components/Section';
-import content from '../data/content';
+import content from '@/data/content';
+import Section from '@/components/Section';
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Will DiBiagio</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          href="https://fonts.googleapis.com/css?family=Cousine:400,700"
-          rel="stylesheet"
-          type="text/css"
-        />
-      </Head>
       <div className="theme" />
       <header className="header">
         <h1>Will DiBiagio</h1>
